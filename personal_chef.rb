4a2f35d94946aa25a472a0b1167b829bcabe9622
Chadwick Flores <chadwickflores@gmail.com>
2014-05-24 23:24:07 -0500

    adding conditional

--- personal_chef.rb
+++ personal_chef.rb
@@ -43,4 +43,17 @@ class PersonalChef
       puts "There are #{quantity} #{item} in the fridge."
     end
   end
+
+  def water_status(minutes)
+    if minutes < 7
+      puts "The water is not boiling yet."
+    elsif minutes == 7
+      puts "It's just barely boiling"
+    elsif minutes == 8
+      puts "It's boiling!"
+    else
+      puts "Hot! Hot! Hot!"
+    end
+    return self
+  end
 end
